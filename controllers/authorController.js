b8dc60853fa58e27615e5896d88dac1e06c78814
Emilien Romulus <emilien.romulus@tuta.io>
2024-10-31 21:51:33 +1100

    added error Handling with asyncHandler

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,6 +1,7 @@
 import * as db from '../mock-db.js';
+import asyncHandler from 'express-async-handler';
 
-async function getAuthorById(req, res) {
+const getAuthorById = asyncHandler(async (req, res) => {
   const { authorId } = req.params;
 
   const author = await db.getAuthorById(Number(authorId));
@@ -11,6 +12,6 @@ async function getAuthorById(req, res) {
   }
 
   res.send(`Author Name: ${author.name}`);
-};
+});
 
 export {getAuthorById};
